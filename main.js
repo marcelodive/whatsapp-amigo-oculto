ab62403955fdb5c365c720f1ed311b24f81ffa8d
Marcelo Pereira Rodrigues <marcelo@Marcelos-MacBook-Pro.local>
2023-10-30 18:14:52 -0300

    Minor adjustments

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const {
+    Client,
+    LocalAuth
+} = require('whatsapp-web.js');
 
 const client = new Client({
     authStrategy: new LocalAuth()
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, {
+        small: true
+    });
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+    console.log('Pronto para receber o comando "\\sortear"');
 });
 
 client.on('message', async msg => {
@@ -19,18 +24,21 @@ client.on('message', async msg => {
 
         if (chat.isGroup) {
             chat.sendMessage('Sorteio iniciado! Em breve vocês receberão o seu amigo oculto no privado!')
-            const participants = chat.participants.filter((participant) => 
+            const participants = chat.participants.filter((participant) =>
                 participant.id._serialized !== msg.to
             );
-            const toDrawParticipants = participants.map(value => ({ value, sort: Math.random() }))
+            const toDrawParticipants = participants.map(value => ({
+                    value,
+                    sort: Math.random()
+                }))
                 .sort((a, b) => a.sort - b.sort)
                 .map(({ value }) => value)
 
-            console.log(participants, toDrawParticipants, '\n\n');
+            for (const participant of participants) {
+                const drawnParticipant = (participant.id._serialized !== toDrawParticipants[0].id._serialized)
+                    ? toDrawParticipants.shift()
+                    : toDrawParticipants.pop();
 
-            for(const participant of participants) {
-                const drawedParticipant = participant.id._serialized !== toDrawParticipants[0].id._serialized ? toDrawParticipants.shift() : toDrawParticipants.pop();
-                console.log(participant.id._serialized, 'tirou', drawedParticipant.id.user)
                 client.sendMessage(participant.id._serialized, `Você tirou...
                 
                 
@@ -93,10 +101,11 @@ client.on('message', async msg => {
                 
                 
                 
-                ...${drawedParticipant.id.user}`)
+                ...${drawnParticipant.id.user}`)
                 await wait();
             }
-            chat.sendMessage('Sorteio finalizado! Confira nossa conversa no privado para saber quem você sorteou!')
+            chat.sendMessage(
+                'Sorteio finalizado! Confira nossa conversa no privado para saber quem você sorteou!')
         }
     }
 });
@@ -109,4 +118,3 @@ async function wait(min = 10000, max = 15000) {
         setTimeout(resolve, randomDelay);
     });
 }
- 
