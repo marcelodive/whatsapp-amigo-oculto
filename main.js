ae125b6359b9cd759604c30289cff6dc8012a4ca
Marcelo Pereira Rodrigues <marcelo@Marcelos-MacBook-Pro.local>
2023-10-30 21:26:33 -0300

    Verify if last element from the arrays are the same

--- main.js
+++ main.js
@@ -1,45 +1,52 @@
 const qrcode = require('qrcode-terminal');
 const {
-    Client,
-    LocalAuth
+  Client,
+  LocalAuth
 } = require('whatsapp-web.js');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth()
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {
-        small: true
-    });
+  qrcode.generate(qr, {
+    small: true
+  });
 });
 
 client.on('ready', () => {
-    console.log('Pronto para receber o comando "\\sortear"');
+  console.log('Pronto para receber o comando "\\sortear"');
 });
 
 client.on('message', async msg => {
-    if (msg.body == '\\sortear') {
-        const chat = await msg.getChat();
+  if (msg.body == '\\sortear') {
+    const chat = await msg.getChat();
 
-        if (chat.isGroup) {
-            chat.sendMessage('Sorteio iniciado! Em breve vocês receberão o seu amigo oculto no privado!')
-            const participants = chat.participants.filter((participant) =>
-                participant.id._serialized !== msg.to
-            );
-            const toDrawParticipants = participants.map(value => ({
-                    value,
-                    sort: Math.random()
-                }))
-                .sort((a, b) => a.sort - b.sort)
-                .map(({ value }) => value)
+    if (chat.isGroup) {
+      chat.sendMessage('Sorteio iniciado! Em breve vocês receberão o seu amigo oculto no privado!')
+      const participants = chat.participants.filter((participant) =>
+        participant.id._serialized !== msg.to
+      );
 
-            for (const participant of participants) {
-                const drawnParticipant = (participant.id._serialized !== toDrawParticipants[0].id._serialized)
-                    ? toDrawParticipants.shift()
-                    : toDrawParticipants.pop();
+      let toDrawParticipants = [];
 
-                client.sendMessage(participant.id._serialized, `Você tirou...
+      do {
+        toDrawParticipants = participants.map(value => ({
+            value,
+            sort: Math.random()
+          }))
+          .sort((a, b) => a.sort - b.sort)
+          .map(({
+            value
+          }) => value)
+      } while (!isLastParticipantEqual(participants, toDrawParticipants));
+
+      for (const participant of participants) {
+        const drawnParticipant = (participant.id._serialized !== toDrawParticipants[0].id._serialized) ?
+          toDrawParticipants.shift() :
+          toDrawParticipants.pop();
+
+        client.sendMessage(participant.id._serialized, `Você tirou...
                 
                 
                 
@@ -102,19 +109,23 @@ client.on('message', async msg => {
                 
                 
                 ...${drawnParticipant.id.user}`)
-                await wait();
-            }
-            chat.sendMessage(
-                'Sorteio finalizado! Confira nossa conversa no privado para saber quem você sorteou!')
-        }
+        await wait();
+      }
+      chat.sendMessage(
+        'Sorteio finalizado! Confira nossa conversa no privado para saber quem você sorteou!')
     }
+  }
 });
 
-client.initialize(); 
+client.initialize();
 
 async function wait(min = 10000, max = 15000) {
-    const randomDelay = Math.floor(Math.random() * (max - min + 1)) + min;
-    return new Promise((resolve) => {
-        setTimeout(resolve, randomDelay);
-    });
+  const randomDelay = Math.floor(Math.random() * (max - min + 1)) + min;
+  return new Promise((resolve) => {
+    setTimeout(resolve, randomDelay);
+  });
+}
+
+function isLastParticipantEqual(participantsA, participantsB) {
+  return participantsA.slice(-1).pop().id._serialized === participantsB.slice(-1).pop().id._serialized;
 }
